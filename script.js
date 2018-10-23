006a553eaf151e8307b00676b18c1f55d27cc908
Njeri <steilemil@gmail.com>
2018-10-23 03:47:15 -0400

    Looks pretty good

--- script.js
+++ script.js
@@ -64,20 +64,28 @@ window.onclick = function(event) {
   }
 }
 
-var modal = document.querySelector(".modal");
-var trigger = document.querySelector(".trigger");
-var closeButton = document.querySelector(".close");
+// Get the modal
+var modal = document.getElementById('myModal');
 
-function toggleModal() {
-    modal.classList.toggle("show-modal");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
 }
 
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
-
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
